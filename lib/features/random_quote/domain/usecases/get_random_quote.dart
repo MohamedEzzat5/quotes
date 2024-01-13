f58b3de67db6c194b37010064d161ef22caf6988
Mohamed Ezzat <MohamedEzzat5>
2024-01-13 13:47:51 +0200

    get random repository done

--- lib/features/random_quote/domain/usecases/get_random_quote.dart
+++ lib/features/random_quote/domain/usecases/get_random_quote.dart
@@ -2,47 +2,17 @@ import 'package:dartz/dartz.dart';
 import 'package:quotes/core/error/failure.dart';
 import 'package:quotes/core/use-cases/usecase.dart';
 import 'package:quotes/features/random_quote/domain/entities/quote.dart';
+import 'package:quotes/features/random_quote/domain/repositories/quote_repository.dart';
 
-class GetRandomQuote implements UseCase<Quote,NoParams>{
+class GetRandomQuote implements UseCase<Quote, NoParams> {
+  final QuoteRepository quoteRepository;
+
+  GetRandomQuote({required this.quoteRepository});
 
   @override
-  Future<Either<Failure,Quote>> call(NoParams params){
-    throw UnimplementedError();
-  }
+  Future<Either<Failure, Quote>> call(NoParams params) =>
+    quoteRepository.getRandomQuote();
+
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class LoginParams extends Equatable{
-//   final String userName;
-//   final String password;
-//
-//   const LoginParams({required this.userName, required this.password});
-//   @override
-//   List<Object?> get props => [userName,password];
-//
-// }
