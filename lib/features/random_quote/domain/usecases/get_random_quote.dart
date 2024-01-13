60b6794b92a24d4d45233c33ac53cd18bb1ec942
Mohamed Ezzat <MohamedEzzat5>
2024-01-13 13:36:28 +0200

    usecases done

---
+++ lib/features/random_quote/domain/usecases/get_random_quote.dart
@@ -0,0 +1,49 @@
+import 'package:dartz/dartz.dart';
+import 'package:quotes/core/error/failure.dart';
+import 'package:quotes/core/use-cases/usecase.dart';
+import 'package:quotes/features/random_quote/domain/entities/quote.dart';
+
+class GetRandomQuote implements UseCase<Quote,NoParams>{
+
+  @override
+  Future<Either<Failure,Quote>> call(NoParams params){
+    throw UnimplementedError();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class LoginParams extends Equatable{
+//   final String userName;
+//   final String password;
+//
+//   const LoginParams({required this.userName, required this.password});
+//   @override
+//   // TODO: implement props
+//   List<Object?> get props => [userName,password];
+//
+// }
