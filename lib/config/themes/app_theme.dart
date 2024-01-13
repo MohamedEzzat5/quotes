faad75ae5f4b7ff3b6273f9f4ad2823696f543c6
Mohamed Ezzat <MohamedEzzat5>
2024-01-13 12:32:57 +0200

    theme is done

--- lib/config/themes/app_theme.dart
+++ lib/config/themes/app_theme.dart
@@ -9,7 +9,19 @@ ThemeData appTheme() {
     brightness: Brightness.light,
     scaffoldBackgroundColor: Colors.white,
     fontFamily: AppStrings.fontFamily,
+    appBarTheme: const  AppBarTheme(
+      centerTitle: true,
+      color: Colors.transparent,
+      elevation: 0,
+      titleTextStyle: TextStyle(color: Colors.black,fontWeight: FontWeight.w500,fontSize: 20),
+
+    ),
     textTheme: const TextTheme(
+      bodyMedium: TextStyle(
+          color: Colors.white,
+          fontSize: 22,
+          fontWeight: FontWeight.w500,
+          height: 1.3),
       labelLarge: TextStyle(
           fontSize: 16,
       color: Colors.black,
