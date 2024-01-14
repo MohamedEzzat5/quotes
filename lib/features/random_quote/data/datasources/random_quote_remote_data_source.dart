7023c8d64b85b78e7606d670fdf757f55c7d00e5
Mohamed Ezzat <MohamedEzzat5>
2024-01-14 07:34:40 +0200

    RandomQuoteRemoteDataSourceImpl done

--- lib/features/random_quote/data/datasources/random_quote_remote_data_source.dart
+++ lib/features/random_quote/data/datasources/random_quote_remote_data_source.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 
-import 'package:quotes/core/api/end_point.dart';
+import 'package:quotes/core/api/end_points.dart';
 import 'package:quotes/core/error/exceptions.dart';
 import 'package:quotes/core/utils/app_strings.dart';
 import 'package:quotes/features/random_quote/data/models/quote_model.dart';
